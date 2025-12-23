05496daf6450b7b41bf1c43f72048e7d3d8fc8be
Adarsh Dwivedi <adarsh.dwivedi@jethitech.com>
2025-12-24 02:56:32 +0530

    connect to MONGO ATLAS

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require("dotenv").config({ path: "./env" });
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+connectDB();
+
+/*
+
+------------- DATA BASE CONNECTION AND SERVER SETUP -------------
+
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("Connected to the database successfully");
+
+    app.on("error", (error) => {
+      console.error("Error in the server:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw error;
+  }
+})();
+
+
+*/
